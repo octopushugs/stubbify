2f8882cbb8592f275eaf00592f96009c376d8248
Ubuntu <ubuntu@ip-172-31-33-59.us-west-2.compute.internal>
2013-12-05 04:52:58 +0000

    backing up for the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Stubbify::Application.routes.draw do
   get "generator/home"
+  resources :generator
+
+  root "generator#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'welcome index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
