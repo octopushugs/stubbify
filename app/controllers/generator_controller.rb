4204b30bbcfdb88a546144edbc7f10d174eb5adb
Ubuntu <ubuntu@ip-172-31-33-59.us-west-2.compute.internal>
2013-12-05 02:21:00 +0000

    initial commit

---
+++ app/controllers/generator_controller.rb
@@ -0,0 +1,4 @@
+class GeneratorController < ApplicationController
+  def home
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stubbify::Application.config.secret_key_base = 'd3149c2766c3a107fac25bcd467027de8f0ff04da43ac8f9c2d409cdc3b4ce6f00edc6b5ca3419984d52c323ff487cf9404b5042be3019653e9f1835c74626fa'
